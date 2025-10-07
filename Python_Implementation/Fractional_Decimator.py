f041f844bb09a062daef9525328f6b1fef2e22d5
amira630 <a.amiraelkomy@outlook.com>
2025-10-07 22:21:06 +0300

    Frac. Deci. w/ interference

--- Python_Implementation/Fractional_Decimator.py
+++ Python_Implementation/Fractional_Decimator.py
@@ -8,9 +8,20 @@ Fs_new = 6e6      # New sampling frequency after decimation (6 MHz)
 Fc = 100e3        # Signal frequency (100 KHz)
 duration = 2e-3 # Signal duration (2 milliseconds)
 
+# --- Interference Parameters ---
+Fi1 = 2.4e6       # First interference frequency (2.4 MHz)
+Fi2 = 5e6         # Second interference frequency (5 MHz)
+A_i1 = 0.5        # Amplitude of interference 1
+A_i2 = 0.3        # Amplitude of interference 2
+
 # --- Time and Signal Generation ---
 t = np.arange(0, duration, 1/Fs)   # Time vector
-x = np.cos(2 * np.pi * Fc * t)     # 9 MHz cosine wave
+x_main = np.cos(2 * np.pi * Fc * t)                # 100 KHz cosine wave
+x_int1 = A_i1 * np.cos(2 * np.pi * Fi1 * t)        # 2.4 MHz interference
+x_int2 = A_i2 * np.cos(2 * np.pi * Fi2 * t)        # 5 MHz interference
+
+# --- Combined Signal ---
+x = x_main + x_int1 + x_int2
 
 # --- Resample using polyphase filtering (2/3 rate) ---
 x_fd = sig.resample_poly(x, up=2, down=3, window=('kaiser', 100.0))
